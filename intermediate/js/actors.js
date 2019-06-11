75295617b2a1bce18c05b365ccb7ed77c9eddddf
Tanja Kunz <tk.kunz@gmx.at>
2019-06-11 15:35:09 +0200

    load json data

--- intermediate/js/actors.js
+++ intermediate/js/actors.js
@@ -3,23 +3,28 @@ $(document).ready(function(){
 	$('.pic').attr('src', actors[0].pic);
 	$('.name').text(actors[0].name);		
 	$('.short').text(actors[0].short);		
-	$('.films').text(films[0].films);		
+	$('.films').text(actors[0].films);		
 	
 	for ( i = 1; i < actors.length; i++) {
 		
-		$('.box:first').clone().appendTo('#films');
+		$('.box:first').clone().appendTo('#cntBox');
 
 		var img = $('.pic');
-		$(img[i]).attr('src', actors[i].pic);
+		$(img[i]).attr('src', actors[i].pic);		
 
-		var movie = $('.movie');
-		$(movie[i]).text(actors[i].name);
+		var name = $('.name');
+		$(name[i]).text(actors[i].name);
+
+		// var id = $(.name[i]).attr('id');
+		var name1 = $('.name').attr('id');
+		$(name1).append(actors[i].name);
 
 		var short = $('.short');
 		$(short[i]).text(actors[i].short);
 
 		var films = $('#filmography');
-		$(filmography[i]).text(data[i].films);
+		$(filmography[i]).text(actors[i].films);
 		
 	};
-}
+	console.log(actors[0].name);
+});
